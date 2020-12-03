8c9574b4c3240bb34b63e6ee6884f592ff8afed3
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2020-12-02 21:13:45 -0500

    Added Multiple todo components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id:1,
+        title: "First ToDo",
+        complete : false
+      },
+      {
+        id:2,
+        title: "Second ToDo",
+        complete : false
+      },
+      {
+        id:3,
+        title: "Third ToDo",
+        complete : false
+      }
+    ]
+  }
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div>
+        <h1>To Do Application</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+
+class Todos extends Component {
+    render() {
+        console.log(this.props.todos)
+        return this.props.todos.map(todo => (
+            <TodoItem key={todo.id} todo={todo}/>
+        ));
+    }
+}
+
+export default Todos
